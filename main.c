ac6ee8b1bdde26d78fa2db6826aafe2e523a0b25
lnn3333 <lna33@sfu.ca>
2024-03-05 23:17:20 -0800

    TODO send, receive, reply

--- main.c
+++ main.c
@@ -1,67 +1,317 @@
 #include "main.h"
-#define SUCCESS 1
-#define FAILURE 0
 
-//Functions
-//Create| C: create a P, add to ready Q 
-//Report Success or Failure, pid of created P
-int create(int priority){
-    PCB *newPCB = (PCB*)malloc(sizeof(PCB));
-    //set Pid, 
-    if (newPCB){
-        return 
+runningP = NULL;
+initP = NULL;
+
+
+// Define helper function to find an available PID
+int findPID()
+{
+    static int availablePID = 0;
+    return availablePID++;
+};
+
+//comparator function
+bool pComparator(void *pItem, void *pComparisonArg){
+    const PCB *PCB_pItem = (PCB *)pItem;
+    const int *int_pComparisonArg = (int *)pComparisonArg;
+
+    return (PCB_pItem->pid == *int_pComparisonArg);
+};
+
+//Search and Kill function
+bool searchAndKill (List *PCBlist, int pidSearch){
+    PCB *pSearch = List_search(PCBlist,pComparator, pidSearch);
+    if (pSearch == NULL){
+        fprintf(stderr, "Error: PID %d does not exist\n", pidSearch);
+        return false;
     }
-}; //(0 = high, 1 = norm, 2 = low)
+    else{
+        if (!List_remove(PCBlist)) {
+            fprintf(stderr, "Error: Couldn't remove process from the queue\n");
+            return false;
+        } else {
+            printf("Success: Removed process with PID %d from the queue\n", pidSearch);
+            return true;
+        }
+    }
+}
 
-//Fork| F: copy current running P, add to ready Q
-//Report Success or Failure, pid of new P
-// attemp to Fork the init P should fail
-int fork();
+// Functions
 
-//Kill| K: kill P, remove P from the system
-//Report Success or Failure
-int kill(int pid);
 
-//Exit| E: kill current running P
-// Reports P scheduling info, which P now get control of CPU
-void exit();
+int createProcess(int priority)
+{
 
-//Quantum| Q: time quantum of running P expire
-//Report action taken (eg. process scheduling information)
+    // Allocate memory for new P
+    PCB *newPCB = (PCB *)malloc(sizeof(PCB));
+    if (newPCB == NULL)
+    {
+        printf("Error create Process\n");
+        return 0;
+    }
+
+    // set Process Pid, state, priority
+    newPCB->pid = findPID();
+    newPCB->priority = priority;
+    newPCB->state = READY;
+    newPCB->messages = List_create();
+    
+    if(newPCB->messages==NULL)
+    {
+        printf("Error create messages list\n");
+        return 0;
+    }
+
+    // Add Process to queue based on priority
+    switch (newPCB->priority)
+    {
+    case (0):
+        List_append(lowPriority, newPCB);
+        break;
+    case (1):
+        List_append(mediumPriority, newPCB);
+        break;
+    case (2):
+        List_append(highPriority, newPCB);
+        break;
+
+    default:
+        printf("Error appending process to queue");
+        break;
+    }
+    printf("Process created with PID: %d\n", newPCB->pid);
+
+    return newPCB->pid;
+};
+
+
+bool forkProcess()
+{
+    // check if Process is init
+    if (runningP == initP){
+        printf("Error: Cannot fork initP");
+        return false;
+    }
+
+    //copy the runningP, add to readyQ
+    PCB* runningPCopy = (PCB*)(malloc(sizeof(PCB)));
+    if (runningPCopy == NULL)
+    {
+        printf("Error create Process\n");
+        return;
+    }
+    memcpy(runningPCopy, runningPCopy, sizeof(PCB));
+    runningPCopy->pid = findPID();
+    List_append(readyQ, runningPCopy);
+
+    printf("Process forked with PID: %d\n", runningPCopy->pid);
+    return true;
+
+
+};
+
+
+bool kill(int pid){
+    //search for pid in readyQ
+    int pPriority = -1;
+    if(List_search(readyQ,pComparator,pid) != NULL){
+        PCB* pSearch = (PCB*) List_search(readyQ,pComparator,pid);
+        searchAndKill(readyQ, pid);
+        pPriority = pSearch->pid;
+        
+    };
+    searchAndKill(readyQ,pid);
+    if(pPriority!= -1){
+        switch (pPriority)
+        {
+        case 0:
+            searchAndKill(lowPriority, pid);
+            break;
+        case 1:
+            searchAndKill(mediumPriority, pid);
+            break;
+        case 2:
+            searchAndKill(highPriority, pid);
+            break;
+        
+        default:
+            break;
+        }
+    }
+    else{
+        // Search and remove from priority queues only
+        if (List_search(lowPriority, pComparator, &pid) != NULL) {
+            pPriority = 0;
+            searchAndKill(lowPriority, pid);
+        } else if (List_search(mediumPriority, pComparator, &pid) != NULL) {
+            pPriority = 1;
+            searchAndKill(mediumPriority, pid);
+        } else if (List_search(highPriority, pComparator, &pid) != NULL) {
+            pPriority = 2;
+            searchAndKill(highPriority, pid);
+        } else {
+            printf("Error: Process with PID %d does not exist\n", pid);
+            return false;
+        }
+    }
+    return true;
+};
+
+void exit(){
+    // check if Process is init
+    if (runningP == initP){
+        printf("Error: Cannot kill initP");
+        return false;
+    }
+    //find P in readyQ
+    int runningPid = runningP->pid;
+    if(runningPid < 0){
+        printf("Error: Cannot get PID of the running process\n");
+        return false;
+    }
+    //Search and Kill running P
+    searchAndKill(readyQ, runningPid);
+    
+    return true;
+
+};
+
+
 int quantum();
 
-//Send| S: send a msg to another P - block until reply
-//Reports Success of Failure, scheduling info, once received the reply: the //msg and source of msg
-int send(int pid,  char *msg);
+
+int send(int pid, char *msg){
+    //add msg to list messages
+    message *new_msg = (message *)malloc(sizeof(message));
+    if (new_msg == NULL) {
+        printf("Error: Memory allocation failed for message\n");
+        return false;
+    }
+
+    //populate new_msg
+    new_msg->pid = pid;
+    strncpy(new_msg->msg, msg, MAX_MSG_LENGTH);
+    new_msg->msg[MAX_MSG_LENGTH]='\0';
+
+    //find pid , append msg to messages
+    PCB *PCBReceiver; 
+    if (List_search(lowPriority, pComparator, &pid) != NULL) {
+        PCBReceiver = List_curr(lowPriority);
+        if(List_append(PCBReceiver->messages, new_msg)==-1){
+            printf("Error: Failed to append message to list\n");
+            free(new_msg); // Free the allocated memory
+            return 0;
+        }        
+    } else if (List_search(mediumPriority, pComparator, &pid) != NULL) {
+        PCBReceiver = List_curr(mediumPriority);
+        if(List_append(PCBReceiver->messages, new_msg)==-1){
+            printf("Error: Failed to append message to list\n");
+            free(new_msg); // Free the allocated memory
+            return 0;
+        }
+        
+    } else if (List_search(highPriority, pComparator, &pid) != NULL) {
+        PCBReceiver = List_curr(highPriority);
+        if(List_append(PCBReceiver->messages, new_msg)==-1){
+            printf("Error: Failed to append message to list\n");
+            free(new_msg); // Free the allocated memory
+            return 0;
+        }
+    } else {
+        printf("Error: Process with PID %d does not exist\n", pid);
+        return false;
+    }
+    //block P , wait until get reply
+    printf("Success send Process with PID %d \n", pid);
+    runningP->state = BLOCKED;
+    return 1;
+
+};
 
 
-//Receive| R: receive a msg - block until one msg arrive
-//Reports scheduling info, once received the reply: the msg and source of msg
-int receive();
+int receive(){
+
+};
+
+int reply(int pid, char *msg); // null terminate string, MAX lenght of msg = 40 char
 
 
-//Reply| Y: unblock sender, deliver the reply
-//Report success or failure
-int reply (int pid, char *msg); //null terminate string, MAX lenght of msg = 40 char
+int new_sem(int semID, int value){
+    if(value < 0 || value > 4){
+        printf("Error: value is not in range");
+        return 0;
+    }
 
-//New sem| N: init name,value of SEM (0-4) (can do only once, error if attempt again) 
-//Report: action taken, Success or Failure
-int new_sem(int semID, int value);
+    sem * newSem = (sem *) malloc(sizeof(sem));
+    if (newSem == NULL){
+        printf("Error: failure create SEM");
+        return 0;
+    }
 
-//sem P |P : execute the semaphore P
-//Report action taken (blocked or not), success or failure.
-int P(int semID); //(semID range 0-4)
+    newSem->semID = semID;
+    newSem->value = value;
+    newSem->pList = List_create();
+    if (newSem->pList == NULL){
+        printf("Error: failure create Process List for SEM");
+        free(newSem); 
+        return 0;
+    }
 
-//sem V |V: execute the semaphore V
-//Report action taken (whether P is readied), success or failure.
-int V(int semID); //(semID range 0-4)
+    return 1;
+};
 
-//Procinfo |I: dump complete state info of P (incl all status)
-//Report see action
+
+int P(int semID){
+
+    if (semID < 0 || semID > 4) {
+        printf("Error: Semaphore ID out of range\n");
+        return 0;
+    }
+    sem* getSem;
+    getSem = &semList[semID];
+    getSem->value -- ;
+     // If semaphore value is negative, block the process
+    if (getSem->value < 0) {
+        // Block the process by adding it to the semaphore's process list
+        if (!List_append(getSem->pList, runningP)) {
+            printf("Error: Failed to add process to semaphore's process list\n");
+            return false;
+        }
+
+        // Suspend the running process
+        suspend();
+    }
+    return true;
+
+
+
+}; //(semID range 0-4)
+
+
+int V(int semID){
+     if (semID < 0 || semID > 4) {
+        printf("Error: Semaphore ID out of range\n");
+        return 0;
+    }
+    sem* getSem;
+    getSem = &semList[semID];
+    getSem->value ++;
+    if (getSem->value <= 0) {
+            // Check if there are processes waiting on the semaphore
+            if (List_count(getSem->pList) > 0) {
+                // Get the first process waiting on the semaphore
+                PCB *waitingProcess = (PCB *)List_remove(getSem->pList);
+
+                resume(waitingProcess);
+            }
+        }
+
+        return 1; // Operation succeeded
+}
+
+
 void proc_info(int pid);
 
-//Totalinfo |T: display all process queues and their content
-//Report see action
+
 void total_info(void);
-
-
--- main.h
+++ main.h
@@ -3,105 +3,111 @@
 #include "list.h"
 #include <stdbool.h>
 #include <unistd.h>
+// Define constants for semaphore IDs
+#define SEMAPHORE_NUMBER 5
+#define MAX_MSG_LENGTH 40
 
-//Define state (0 : ready, 1 : running, 2 : blocked, 3 : deadlock) 
-enum processState {
+// Define state (0 : ready, 1 : running, 2 : blocked, 3 : deadlock)
+enum processState
+{
     READY,
     RUNNING,
     BLOCKED,
     DEADLOCK
 };
 
-//Define priority level (high 2, medium 1, low 0)
-enum processPriority{
-    LOW,
-    MEDIUM,
-    HIGH
-};
-
-//Process Block Control data structure
+// Process Block Control data structure
 typedef struct PCB_s PCB;
 struct PCB_s
 {
     int pid;
-	processPriority priority; 
-	processState state; 
+    int priority;
+    processState state;
+    List* messages;
 };
 
-//Semaphore data structure
+// Semaphore data structure
 
 typedef struct sem_S sem;
-struct sem_S 
+struct sem_S
 {
+    int semID;
     int value;
     PCB *pList;
 };
 
-//Ready Q based on priority (high 2, medium 1, low 0)
-List_s *highPriority;
-List_s *mediumPriority;
-List_s *lowPriority;
+// Define a structure to represent a message
+typedef struct message_S message;
+struct message_S {
+    int pid;
+    char msg[MAX_MSG_LENGTH + 1]; // +1 for null terminator
+};
+
+// Ready Q based on priority (high 2, medium 1, low 0)
+List *highPriority;
+List *mediumPriority;
+List *lowPriority;
 
 // Queue wait for send msg
-List_s *jobQ;
+List *jobQ;
 
 // Queue wait for receive msg
-List_s *readyQ;
+List *readyQ;
 
-//Functions
-//Create| C: create a P, add to ready Q 
-//Report Success or Failure, pid of created P
-int create(int priority); //(0 = high, 1 = norm, 2 = low)
+PCB *runningP;
+PCB *initP;
+sem semList[5];
+List *messages;
+// Functions
+// Create| C: create a P, add to ready Q
+// Report Success or Failure, pid of created P
+int createProcess(int priority); //(0 = high, 1 = norm, 2 = low)
 
-//Fork| F: copy current running P, add to ready Q
-//Report Success or Failure, pid of new P
-// attemp to Fork the init P should fail
-int fork();
+// Fork| F: copy current running P, add to ready Q
+// Report Success or Failure, pid of new P
+//  attemp to Fork the init P should fail
+bool forkProcess();
 
-//Kill| K: kill P, remove P from the system
-//Report Success or Failure
-int kill(int pid);
+// Kill| K: kill P, remove P from the system
+// Report Success or Failure
+bool kill(int pid);
 
-//Exit| E: kill current running P
-// Reports P scheduling info, which P now get control of CPU
+// Exit| E: kill current running P
+//  Reports P scheduling info, which P now get control of CPU
 void exit();
 
-//Quantum| Q: time quantum of running P expire
-//Report action taken (eg. process scheduling information)
+// Quantum| Q: time quantum of running P expire
+// Report action taken (eg. process scheduling information)
 int quantum();
 
-//Send| S: send a msg to another P - block until reply
-//Reports Success of Failure, scheduling info, once received the reply: the //msg and source of msg
-int send(int pid,  char *msg);
+// Send| S: send a msg to another P - block until reply
+// Reports Success of Failure, scheduling info, once received the reply: the //msg and source of msg
+int send(int pid, char *msg);
 
-
-//Receive| R: receive a msg - block until one msg arrive
-//Reports scheduling info, once received the reply: the msg and source of msg
+// Receive| R: receive a msg - block until one msg arrive
+// Reports scheduling info, once received the reply: the msg and source of msg
 int receive();
 
+// Reply| Y: unblock sender, deliver the reply
+// Report success or failure
+int reply(int pid, char *msg); // null terminate string, MAX lenght of msg = 40 char
 
-//Reply| Y: unblock sender, deliver the reply
-//Report success or failure
-int reply (int pid, char *msg); //null terminate string, MAX lenght of msg = 40 char
-
-//New sem| N: init name,value of SEM (0-4) (can do only once, error if attempt again) 
-//Report: action taken, Success or Failure
+// New sem| N: init name,value of SEM (0-4) (can do only once, error if attempt again)
+// Report: action taken, Success or Failure
 int new_sem(int semID, int value);
 
-//sem P |P : execute the semaphore P
-//Report action taken (blocked or not), success or failure.
+// sem P |P : execute the semaphore P
+// Report action taken (blocked or not), success or failure.
 int P(int semID); //(semID range 0-4)
 
-//sem V |V: execute the semaphore V
-//Report action taken (whether P is readied), success or failure.
+// sem V |V: execute the semaphore V
+// Report action taken (whether P is readied), success or failure.
 int V(int semID); //(semID range 0-4)
 
-//Procinfo |I: dump complete state info of P (incl all status)
-//Report see action
+// Procinfo |I: dump complete state info of P (incl all status)
+// Report see action
 void proc_info(int pid);
 
-//Totalinfo |T: display all process queues and their content
-//Report see action
+// Totalinfo |T: display all process queues and their content
+// Report see action
 void total_info(void);
-
-
